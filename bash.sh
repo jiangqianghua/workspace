c9f50f2030b534ee732fd918fb5f5dc905267f57
jiangqianghua <240437339@qq.com>
2015-01-22 19:52:41 -0800

    add file

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: bash.sh
+# Author: qianghua jiang
+# mail: 240437339@qq.com
+# Created Time: Thu 22 Jan 2015 07:48:19 PM PST
+#########################################################################
+#!/bin/bash
+git add .
+git commit -a -m 'add file'
+git push -u origin master
