6156ef57853dd019bd2d06d53885affef02dd57b
jiangqianghua <240437339@qq.com>
2015-01-22 20:03:38 -0800

    add redis

---
+++ redis/test/redisTest.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <hiredis/hiredis.h>
+int main()
+{
+        redisContext* conn = redisConnect("127.0.0.1",6379);
+        if(conn->err)
+        {
+                printf("connecion error:%s\n",conn->errstr);
+        }
+        redisReply* reply = (redisReply*)redisCommand(conn,"set foo 1234");
+        freeReplyObject(reply);
+        reply = (redisReply*)redisCommand(conn,"get foo");
+        printf("%s\n",reply->str);
+        freeReplyObject(reply);
+
+        redisFree(conn);
+        return 0 ;
+}
