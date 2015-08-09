0fa40663928f6d1a514d2bcd66763338d2e8e41f
jiangqianghua <240437339@qq.com>
2015-08-09 08:07:12 -0700

    add file

---
+++ web/bootstrap/copy.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: copy1.sh
+# Author: qianghua jiang
+# mail: 240437339@qq.com
+# Created Time: Sun 09 Aug 2015 08:03:58 AM PDT
+#########################################################################
+#!/bin/bash
+cp -rf * /home/jiangqianghua/Desktop/workspace/web/bootstrap
