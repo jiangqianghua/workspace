379593ee5749fa4be7de767ab404f7721a509c53
jiangqianghua <240437339@qq.com>
2015-12-18 22:36:32 -0800

    add file

---
+++ node/node3.1/test-currency.js
@@ -0,0 +1,6 @@
+var currency = require('./currency')
+
+console.log('50 Canadian dollars equals this amount of us dollars');
+console.log(currency.canadianToUS(50))
+console.log('30 Us dollars equals this amount of Canadian dollars');
+console.log(currency.USToCanadian(30))
